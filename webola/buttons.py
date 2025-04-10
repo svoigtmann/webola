a977cc2384cf6f288bb90e39d35865e177237a49
Steffen Voigtmann <steffen.voigtmann@bht-berlin.de>
2025-04-10 07:22:05 +0200

    decrease button height for improved display

--- webola/buttons.py
+++ webola/buttons.py
@@ -18,77 +18,38 @@ class NoFocusButton(QPushButton):
         if slot is None: slot = lambda: print(text)
         if slot:         self.clicked.connect(slot) # allow to set no slot by providing False
 
-#class ButtonLabel(QLabel):
-#    def __init__(self, label, parent):
-#        QLabel.__init__(self, parent)
-#        self.setText(label)
-#        #self.setMargin(0)
-#        #self.setContentsMargins(0,0,0,0)
-#        #self.setAlignment(Qt.AlignCenter)
-#        
-#    def mousePressEvent(self, event):
-#        if event.button() == Qt.LeftButton:
-#            self.parent().click()           
-#
+class ButtonLabel(QLabel):
+    def __init__(self, label, parent):
+        QLabel.__init__(self, parent)
+        self.setText(label)
 
-#class ButtonLabel(QWidget):
-#    def __init__(self, text, parent):
-#        super().__init__()
-#        self.parent = parent
-#        
-#    def setText(self, text):
-#        pass
-#        
-#class SubtitleButton(NoFocusButton):
-#    def __init__(self, slot=None, parent=None):
-#        NoFocusButton.__init__(self, '', self.was_clicked, parent)        
-#        
-#        self.label= QLabel(self)
-#        self.label.setStyleSheet('background-color: lightblue;border-width: 0px;')
-#        
-#        self.main = ButtonLabel('', self)
-#        self.sub  = ButtonLabel('', self)
-#        #
-#        #self.main.setStyleSheet('background-color: lightblue;border-width: 0px;')
-#        #self.sub.setStyleSheet('background-color: lightgreen;border-width: 0px;')
-#        ##self.main.setContentsMargins(0,0,0,0)
-#        ##self.sub.setContentsMargins(0,0,0,0)
-#        ##self.main.setAlignment(Qt.AlignBottom)
-#        #self.sub.setAlignment(Qt.AlignTop)        
-#        ##self.setStyleSheet("QPushButton {border-style: outset; border-width: 0px;}")
-#        #self.setStyleSheet( "padding: 0px; border:0px;" );
-#        #
-#        #v = QVBoxLayout(self)
-#        #f = VBoxContainer()
-#        #f.layout().setSpacing(0)
-#        #f.layout().addStretch()
-#        #f.setContentsMargins(0, 0, 0, 0)
-#        #v.addWidget(f)
-#        #f.add(self.main,stretch=4)
-#        #f.add(self.sub,stretch=3)
-#        #f.layout().addStretch()
-#        ##v.setSpacing(0)
-#        ###v.setContentsMargins(0,0,0,0)
-#        ##v.addStretch()
-#        ##v.addWidget(self.main)
-#        ##v.addWidget(self.sub )
-#        ##v.addStretch()
-#        #self.setLayout(v)
-#
-#    def scale_fonts(self, new):
-#        self.setMinimumHeight(round(70+5*(new-10))) # 10pt => 70, 12pt => 80
-#        for w,fac in zip((self.main, self.sub),(1.125, 0.8)):
-#            f = w.font()
-#            f.setPointSize(round(new*fac)) 
-#            w.setFont(f)
-#
-#    def setText(self, text, sub=None):
-#        #two = "<font size=18>%s</font><br><b><font size=24>%s</font></b>" % (text, sub or '')
-#        two = "%s<br>%s" % (text, sub or '')
-#        self.label.setText(two)
-#        
-#        
-#        
-#        #self.main.setText(text)
-#        #if sub is not None: self.sub.setText(sub)
-#
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.parent().click()           
+
+class SubtitleButton(NoFocusButton):
+    def __init__(self, slot=None, parent=None):
+        NoFocusButton.__init__(self, '', self.was_clicked, parent)        
+        
+        self.main = ButtonLabel('', self)
+        self.sub  = ButtonLabel('', self)
+
+        self.main.setAlignment(Qt.AlignBottom)
+        self.sub.setAlignment(Qt.AlignTop)        
+        
+        v = QVBoxLayout(self)
+        v.setSpacing(0)
+        v.addWidget(self.main)
+        v.addWidget(self.sub )
+        self.setLayout(v)
+
+    def scale_fonts(self, new):
+        self.setMinimumHeight(round(45+5*(new-10))) # 10pt => 45, 12pt => 55
+        for w,fac in zip((self.main, self.sub),(1.125, 0.8)):
+            f = w.font()
+            f.setPointSize(round(new*fac)) 
+            w.setFont(f)
+
+    def setText(self, text, sub=None):
+        self.main.setText(text)
+        if sub is not None: self.sub.setText(sub)
