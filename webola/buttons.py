346340fbf5daa40460a11971db4864023d178b96
Steffen Voigtmann <svoigtmann@bht-berlin.de>
2025-04-26 20:03:06 +0200

    decide button size on os type

--- webola/buttons.py
+++ webola/buttons.py
@@ -1,4 +1,5 @@
 from PyQt5.Qt import Qt, QPushButton, QLabel, QVBoxLayout, QToolButton, QIcon
+from webola.utils import is_linux
 
 class ToolButton(QToolButton):
     def __init__(self, name, slot=None, tip=None, parent=None):
@@ -42,8 +43,11 @@ class SubtitleButton(NoFocusButton):
         self.setLayout(v)
 
     def scale_fonts(self, new):
-        #self.setMinimumHeight(round(45+5*(new-10))) # 10pt => 45, 12pt => 55
-        self.setMinimumHeight(round(70+5*(new-10))) # 10pt => 70, 12pt => 80 
+        if is_linux():
+            self.setMinimumHeight(round(45+5*(new-10))) # 10pt => 45, 12pt => 55
+        else:
+            self.setMinimumHeight(round(70+5*(new-10))) # 10pt => 70, 12pt => 80 
+        
         for w,fac in zip((self.main, self.sub),(1.125, 0.8)):
             f = w.font()
             f.setPointSize(round(new*fac)) 
